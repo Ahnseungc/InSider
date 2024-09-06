4c0fe5acc1b3d8fbdcd6ae1c43590e116a0757ad
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-09-06 23:08:42 +0900

    FIx .

--- src/components/home/weekTopic/topicList.tsx
+++ src/components/home/weekTopic/topicList.tsx
@@ -1,14 +1,10 @@
-'use client';
+import { FC } from 'react';
 
-import { FC, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
 import { TopicCard } from './topicCard';
-import { LeftArrow, RightArrow } from '@/components/common/Icon';
 
 export const TopicList: FC = () => {
   const test = [1, 2, 3];
-  const [currentCard, setCurrentCard] = useState<number>(0);
+
   return (
     <div className="flex gap-6">
       {test.map(item => {
